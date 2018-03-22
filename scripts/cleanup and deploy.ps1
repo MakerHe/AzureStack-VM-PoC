b8091036da4d3be6805329b5b1499055bd7d76af
Maker He <maker.he@whinniy.com>
2018-03-22 08:45:53 +0800

    Change paramater

--- scripts/cleanup and deploy.ps1
+++ scripts/cleanup and deploy.ps1
@@ -22,23 +22,23 @@ Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroupName | Remove-AzureRm
 Remove-AzureRmResourceGroup -ResourceGroupName $resourceGroupName -Force
 
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
-  -TemplateUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.json `
-  -TemplateParameterUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.parameters.jsonRemove-AzureRmResourceGroup -Force
+  -TemplateUri https://raw.githubusercontent.com/MakerHe/yagmurs_AzureVMExtension/master/azuredeploy.json `
+  -TemplateParameterUri https://raw.githubusercontent.com/MakerHe/yagmurs_AzureVMExtension/master/azuredeploy.parameters.jsonRemove-AzureRmResourceGroup -Force
 
 New-AzureRmResourceGroup -Name $resourceGroupName -Location 'west europe'
 New-AzureRmResourceGroupDeployment -Name "$resourceGroupName-PoC-Deployment" -ResourceGroupName $resourceGroupName `
-  -TemplateUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.json `
-  -TemplateParameterUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.parameters.json `
+  -TemplateUri https://raw.githubusercontent.com/MakerHe/yagmurs_AzureVMExtension/master/azuredeploy.json `
+  -TemplateParameterUri https://raw.githubusercontent.com/MakerHe/yagmurs_AzureVMExtension/master/azuredeploy.parameters.json `
   -Mode Incremental
 
 New-AzureRmResourceGroupDeployment -Name "$resourceGroupName-PoC-Deployment" -ResourceGroupName $resourceGroupName `
-  -TemplateUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.json `
+  -TemplateUri https://raw.githubusercontent.com/MakerHe/yagmurs_AzureVMExtension/master/azuredeploy.json `
   -Mode Incremental
 
 
 $swName = "NATSwitch"
 New-VMSwitch -Name $swName -SwitchType Internal -Verbose
 $NIC=Get-NetAdapter "vEthernet `($swName`)"
-New-NetIPAddress -IPAddress 172.16.0.1 -PrefixLength 24 -InterfaceIndex $NIC.ifIndex
-New-NetNat -Name $swName -InternalIPInterfaceAddressPrefix "172.16.0.0/24" –Verbose
+New-NetIPAddress -IPAddress 172.21.240.1 -PrefixLength 24 -InterfaceIndex $NIC.ifIndex
+New-NetNat -Name $swName -InternalIPInterfaceAddressPrefix "172.21.240.0/24" –Verbose
 Get-VM -Name AzS-BGPNAT01 | Get-VMNetworkAdapter -Name NAT | Connect-VMNetworkAdapter -SwitchName $swName
